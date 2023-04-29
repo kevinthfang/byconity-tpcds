680c6c9e6176c6d1f5c30496d42ada7cdbd3ea6d
kevinthfang <kevinth.fang@gmail.com>
2023-04-29 15:22:24 +0000

    clickhouse scripts

--- helper.sh
+++ helper.sh
@@ -20,7 +20,6 @@ TRACE_LOG=${LOGDIR}/trace.log
 TOOLS_PATH=${SCRIPTPATH}/build/tools
 SUITE=${SUITE:-tpcds}
 CLUSTER_NAME=${CLUSTER_NAME:-cluster}
-CLIENT_TYPE="ByConity"
 ENABLE_TRACE="true"
 
 if [ -n "$SRV_USER" ]; then
@@ -30,9 +29,6 @@ if [ -n "$SRV_USER" ]; then
         USER_OPTS="--user ${SRV_USER} --password \"\""
     fi
 fi
-
-OPTIMIZER_OPTS=" --enable_optimizer=1 --dialect_type='ANSI'"
-OPTIMIZER_SETS="set enable_optimizer=1; set dialect_type='ANSI'; "
 
 if [ ! -d $LOGDIR ]; then
     mkdir -p $LOGDIR
@@ -48,13 +44,8 @@ function trace() {
 
 # call clickhouse client to execute sql, $1 has the sql, addtional parameters will be added to the clickhouse client
 function clickhouse_client_cmd() {
-    if [ $CLIENT_TYPE == "CH" ]; then
-        QUERY=$(sed 's|\"|\\\"|g' <<<"${OPTIMIZER_SETS} $1")
-        echo "$SCRIPTPATH/bin/clickhouse client $CLIENT_ARGS --distributed_product_mode=allow --send_timeout=2147483647 --receive_timeout=2147483647 $USER_OPTS --port=$SRV_TCP_PORT --host=$SRV_IP -mn -q \"$QUERY\" ${@:2}"
-    else
-        QUERY=$(sed 's|\"|\\\"|g' <<<"$1")
-        echo "$SCRIPTPATH/bin/clickhouse client $CLIENT_ARGS $OPTIMIZER_OPTS --distributed_product_mode=allow --send_timeout=2147483647 --receive_timeout=2147483647 $USER_OPTS --port=$SRV_TCP_PORT --host=$SRV_IP -mn -q \"$QUERY\" ${@:2}"
-    fi
+    QUERY=$(sed 's|\"|\\\"|g' <<<"$1")
+    echo "$SCRIPTPATH/bin/clickhouse client $CLIENT_ARGS --distributed_product_mode=allow --send_timeout=2147483647 --receive_timeout=2147483647 $USER_OPTS --port=$SRV_TCP_PORT --host=$SRV_IP -mn -q \"$QUERY\" ${@:2}"
 }
 function clickhouse_client() {
     CMD=$(clickhouse_client_cmd "$1" "${@:2}")
